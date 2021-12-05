987abb3f1c67257d0a09f9c9aaa71f6c9ed106a4
Amal Ashok <amalashokunni345@gmail.com>
2021-12-05 08:50:48 -0800

    add another file

--- java2_Rectangle.java
+++ java2_Rectangle.java
@@ -1,34 +1,40 @@
-class Rectangle
-{
+import java.util.*;
+
+class Rectangle {
     double length;
     double breadth;
-    void setData(double l,double b)
-    {
-        length=l;
-        breadth=b;
+
+    void setData(double l, double b) {
+        length = l;
+        breadth = b;
     }
-    double getArea()
-    {
-        double  area;
-        area=length*breadth;
+
+    double getArea() {
+        double area;
+        area = length * breadth;
         return area;
     }
 }
-class java2_Rectangle
-{
-    public static void main(String args[])
-    {
-        double Area;
-        Rectangle R1=new Rectangle();
-        R1.setData(12.4,13);
-        Area=R1.getArea();
-        System.out.println("Area of Rectangle,R1 = "+Area);
-        Rectangle R2=new Rectangle();
-        R2.setData(10.5,9);
-        Area=R2.getArea();
-        System.out.println("Area of Rectangle,R2 = "+Area);
-        Rectangle R3=new Rectangle();
-        R3.setData(13,14);
-        Area=R3.getArea();
-        System.out.println("Area of Rectangle,R3 = "+Area);
-    }}
+
+class java2_Rectangle {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        double Area, l, b;
+        Rectangle R1 = new Rectangle();
+        System.out.println("Enter the length and breadth of reactangle:");
+        l = sc.nextInt();
+        b = sc.nextInt();
+        R1.setData(l, b);
+        Area = R1.getArea();
+        System.out.println("Area of Rectangle,R1 = " + Area);
+        Rectangle R2 = new Rectangle();
+        R2.setData(10.5, 9);
+        Area = R2.getArea();
+        System.out.println("Area of Rectangle,R2 = " + Area);
+        Rectangle R3 = new Rectangle();
+        R3.setData(13, 14);
+        Area = R3.getArea();
+        System.out.println("Area of Rectangle,R3 = " + Area);
+        sc.close();
+    }
+}
